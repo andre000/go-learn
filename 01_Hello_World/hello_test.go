20d26e014b23ff89e2aa4ad565e03e0216bb7b73
Andre Adriano <andre@dev-next.com>
2019-08-16 11:21:50 -0300

    ✨ Added 'Hello World'

---
+++ 01_Hello_World/hello_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assert := func(t *testing.T, received, expected string) {
+		// t.Helper() is needed to tell the test suite that this method is a helper
+		// By doing this when it fails the line number reported will be in our
+		// function call rather than inside our test helper
+		t.Helper()
+		if received != expected {
+			t.Errorf("❌ received %q expected %q", received, expected)
+		}
+	}
+
+	t.Run("should say hello to people", func(t *testing.T) {
+		received := Hello("Andre")
+		expected := "Hello, Andre!"
+
+		assert(t, received, expected)
+	})
+
+	t.Run("should say 'Hello, Stranger!' when an empty string is provided", func(t *testing.T) {
+		received := Hello("")
+		expected := "Hello, Stranger!"
+
+		assert(t, received, expected)
+	})
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# go-learn 🐹
+
+Project with my learnings on go. With the [learn-go-with-tests](https://github.com/quii/learn-go-with-tests) as a guide.
+
